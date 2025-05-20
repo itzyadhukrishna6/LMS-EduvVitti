25040bbab50fe9e87085839be8ba3efdb7958883
itzyadhukrishna6 <yadhukrishnan0718@gmnail.com>
2025-05-21 01:29:03 +0530

    first commit

--- client/src/components/educator/Footer.jsx
+++ client/src/components/educator/Footer.jsx
@@ -8,17 +8,17 @@ const Footer = () => {
         <img className='hidden md:block w-20' src={assets.logo} alt="logo" />
         <div className='hidden md:block h-7 w-px bg-gray-500/60'></div>
         <p className="py-4 text-center text-xs md:text-sm text-gray-500">
-          Copyright 2025 © GreatStack. All Right Reserved.
+          Copyright 2025 © Eduvritti. All Right Reserved.
         </p>
       </div>
       <div className='flex items-center gap-3 max-md:mt-4'>
-        <a href="#">
+        <a href="https://www.facebook.com/share/1AUUevDw1x/">
           <img src={assets.facebook_icon} alt="facebook_icon" />
         </a>
         <a href="#">
           <img src={assets.twitter_icon} alt="twitter_icon" />
         </a>
-        <a href="#">
+        <a href="https://www.instagram.com/edu.vritti?igsh=dWx5YXF4ZmYyZ2Uy">
           <img src={assets.instagram_icon} alt="instagram_icon" />
         </a>
       </div>
--- client/src/components/student/TestimonialsSection.jsx
+++ client/src/components/student/TestimonialsSection.jsx
@@ -5,7 +5,7 @@ const TestimonialsSection = () => {
 
   return (
     <div className="pb-14 px-8 md:px-0">
-      <h2 className="text-3xl font-medium text-gray-800">Testimonials</h2>
+      <h2 className="text-3xl font-medium text-gray-800">User Experience</h2>
       <p className="md:text-base text-gray-500 mt-3">
         Hear from our learners as they share their journeys of transformation, success, and how our <br /> platform has made a difference in their lives.
       </p>
