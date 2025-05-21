1299afe44006ea75a390a72d81d3ae5677fa9cb0
Yadhukrishanan KV <yadhukrishnan0718@gmail.com>
2025-05-21 15:16:01 +0530

    Update CoursesList.jsx

--- client/src/pages/student/CoursesList.jsx
+++ client/src/pages/student/CoursesList.jsx
@@ -38,7 +38,7 @@ const CoursesList = () => {
                 <div className='flex md:flex-row flex-col gap-6 items-start justify-between w-full'>
                     <div>
                         <h1 className='text-4xl font-semibold text-gray-800'>Course List</h1>
-                        <p className='text-gray-500'><span onClick={() => navigate('/')} className='text-blue-600 cursor-pointer'>Home</span> / <span>Course List</span></p>
+                        <p className='text-gray-500'><span onClick={() => navigate('/')} className='text-orange-600 cursor-pointer'>Home</span> / <span>Course List</span></p>
                     </div>
                     <SearchBar data={input} />
                 </div>
